39619ebbba0debce2762c553e3affeec6a6a0ae7
EDWIN ORBES <yovany.dev@gmail.com>
2018-07-02 22:36:13 -0500

    UI del formulario para crear cuenta terminado

--- app/src/main/java/com/yovanydev/platzigram/view/CreateAccountActivity.java
+++ app/src/main/java/com/yovanydev/platzigram/view/CreateAccountActivity.java
@@ -2,6 +2,7 @@ package com.yovanydev.platzigram.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.yovanydev.platzigram.R;
 
@@ -11,5 +12,13 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        showToolbar(getResources().getString(R.string.toolbar_title_create_account), true);
+    }
+
+    public void showToolbar(String title, Boolean upButton) {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 }
