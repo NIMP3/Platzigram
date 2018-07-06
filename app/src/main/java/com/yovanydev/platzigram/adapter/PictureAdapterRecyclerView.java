1c728ad0a813cef9cb2560753727228700993dc1
EDWIN ORBES <yovany.dev@gmail.com>
2018-07-06 17:37:48 -0500

    Implementación de RecyclerView, Adapter y POJOS para el listado de Imagenes en el Home

---
+++ app/src/main/java/com/yovanydev/platzigram/adapter/PictureAdapterRecyclerView.java
@@ -0,0 +1,72 @@
+package com.yovanydev.platzigram.adapter;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+import com.yovanydev.platzigram.R;
+import com.yovanydev.platzigram.model.Picture;
+
+import java.util.ArrayList;
+
+public class PictureAdapterRecyclerView extends RecyclerView.Adapter<PictureAdapterRecyclerView.PictureViewHolder>{
+
+    private ArrayList<Picture> pictures;
+    private int resource;
+    private Activity activity;
+
+    public PictureAdapterRecyclerView(ArrayList<Picture> pictures, int resource, Activity activity) {
+        this.pictures = pictures;
+        this.resource = resource;
+        this.activity = activity;
+    }
+
+    //Inflamos nuestro cardview en la vista
+    @NonNull
+    @Override
+    public PictureViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(resource, viewGroup, false);
+        return new PictureViewHolder(view);
+    }
+
+    //Aqui se trabaja con la lista de Elementos
+    @Override
+    public void onBindViewHolder(@NonNull PictureViewHolder pictureViewHolder, int i) {
+        Picture picture = pictures.get(i);
+
+        Picasso.get().load(picture.getPicture()).into(pictureViewHolder.pictureCard);
+        pictureViewHolder.usernameCard.setText(picture.getUsername());
+        pictureViewHolder.timeCard.setText(picture.getTime());
+        pictureViewHolder.likeNumberCard.setText(picture.getLikeNumber());
+    }
+
+    //Tamaño de la lista de Elementos
+    @Override
+    public int getItemCount() {
+        return pictures.size();
+    }
+
+    public class PictureViewHolder extends RecyclerView.ViewHolder {
+
+        //Views del Cardview
+        private ImageView pictureCard;
+        private TextView usernameCard;
+        private TextView timeCard;
+        private TextView likeNumberCard;
+
+        public PictureViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            pictureCard = itemView.findViewById(R.id.ivPictureCard);
+            usernameCard = itemView.findViewById(R.id.tvUsernameCard);
+            timeCard = itemView.findViewById(R.id.tvTimeCard);
+            likeNumberCard = itemView.findViewById(R.id.tvLikeNumberCard);
+        }
+    }
+}
--- app/src/main/java/com/yovanydev/platzigram/view/fragment/HomeFragment.java
+++ app/src/main/java/com/yovanydev/platzigram/view/fragment/HomeFragment.java
@@ -3,12 +3,20 @@ package com.yovanydev.platzigram.view.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.yovanydev.platzigram.R;
+import com.yovanydev.platzigram.adapter.PictureAdapterRecyclerView;
+import com.yovanydev.platzigram.model.Picture;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +33,41 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        showToolbar(getResources().getString(R.string.titletab_home), false, view);
+
+        RecyclerView picturesRecycler = view.findViewById(R.id.recyclerviewPicture);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(linearLayoutManager.VERTICAL);
+        picturesRecycler.setLayoutManager(linearLayoutManager);
+
+        PictureAdapterRecyclerView pictureAdapterRecyclerView =
+                new PictureAdapterRecyclerView(buildPictures(),R.layout.cardview_picture, getActivity());
+
+        picturesRecycler.setAdapter(pictureAdapterRecyclerView);
+
+        return view;
+    }
+
+    public ArrayList<Picture> buildPictures() {
+        ArrayList<Picture> pictures = new ArrayList<>();
+
+        pictures.add(new Picture("http://www.novalandtours.com/images/guide/guilin.jpg","Edwin Yovany","4 días", "4 Me Gusta"));
+        pictures.add(new Picture("https://miviaje.com/wp-content/uploads/2016/05/shutterstock_250683580.jpg","Carolina Fernandes","1 día", "1 Me Gusta"));
+        pictures.add(new Picture("https://i.imgur.com/PQZ7otL.jpg","Jesús Insuasty","2 días", "10 Me Gusta"));
+        pictures.add(new Picture("https://i.imgur.com/5d2HHEE.jpg","Estefania Burbano","14 días", "50 Me Gusta"));
+        pictures.add(new Picture("https://i.imgur.com/cmEqJOv.jpg","Felipe Muñoz","50 días", "100 Me Gusta"));
+
+        return pictures;
+    }
+
+    //Mostrar el Toolbar en la vista
+    public void showToolbar(String title, Boolean upButton, View view) {
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(title);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 }
